1e13e35dbdfdfaa9c9b0e8da2f9bcba89200320a
Byeong Min Cho <merrybmc@gmail.com>
2024-06-20 00:06:50 +0900

    feat : 이메일 회원가입 Schema 정의 (#3)

--- entitys/user/controller/user.controller.js
+++ entitys/user/controller/user.controller.js
@@ -1,3 +1,5 @@
+import User from '../user.Schema';
+
 const userController = {};
 
 export default userController;
--- entitys/user/user.Schema.js
+++ entitys/user/user.Schema.js
@@ -2,7 +2,20 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-const userSchema = Schema({});
+const userSchema = Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+  kind: { type: String },
+  image: { type: String },
+});
+
+userSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.password;
+  delete obj.__v;
+  return obj;
+};
 
 const User = mongoose.model('User', userSchema);
 
