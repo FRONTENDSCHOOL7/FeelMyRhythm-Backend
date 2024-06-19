24674c79b506a8f1b75881ab07dc4f4032408f55
Byeong Min Cho <merrybmc@gmail.com>
2024-06-20 00:14:13 +0900

    feat : intercepter 설정 (#3)

--- common/exception/http-exception.filter.js
+++ common/exception/http-exception.filter.js
@@ -1,3 +1,8 @@
-const intercepter = async (req, res) => {};
+const intercepter = async (req, res) => {
+  const { data, error } = req;
+
+  if (req.statusCode === 200) res.status(200).json({ status: 'success', data });
+  if (req.statusCode === 400) res.status(400).json({ status: 'fail', error });
+};
 
 export default intercepter;
