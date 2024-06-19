1417ce87d5bd35370ea1b611a346b489140ddc2e
Byeong Min Cho <merrybmc@gmail.com>
2024-06-20 00:13:01 +0900

    feat : 기본 회원가입 로직 구현 (#3)

--- entitys/user/user.Repository.js
+++ entitys/user/user.Repository.js
@@ -1,3 +1,44 @@
+import User from './user.Schema';
+import bcrypt from 'bcryptjs';
+
 const userRepository = {};
+
+// 이메일 검증
+userRepository.validEmail = async (req, res, next) => {
+  try {
+    if (req.statusCode === 400) return next();
+
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user) {
+      throw new Error('이미 가입된 이메일입니다.');
+    }
+
+    req.user = user;
+  } catch (e) {
+    req.statusCode = 400;
+    req.error = e.message;
+  }
+  next();
+};
+
+// 비밀번호 암호화
+userRepository.hashPassword = async (req, res, next) => {
+  try {
+    if (req.statusCode === 400) return next();
+
+    const { password } = req.body;
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    req.hashPassword = hash;
+  } catch (e) {
+    req.statusCode = 400;
+    req.error = e.message;
+  }
+  next();
+};
 
 export default userRepository;
--- entitys/user/user.api.js
+++ entitys/user/user.api.js
@@ -1,5 +1,18 @@
 import express from 'express';
+import userRepository from './user.Repository';
+import userController from './controller/user.controller';
+import userService from './service/user.service';
 
 const router = express.Router();
+
+// 회원가입
+router.post(
+  '/emailregister',
+  userRepository.validEmail,
+  userController.emailRegister,
+  userRepository.hashPassword,
+  userService.emailRegister,
+  intercepter
+);
 
 export default router;
