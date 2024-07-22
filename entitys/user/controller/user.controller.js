8f92364b12751140d6cb6b5731b69db2eb0f8ee8
merrybmc <merrybmc@gmail.com>
2024-07-22 17:49:03 +0900

    feat : 닉네임 검증 (#16)

--- entitys/user/controller/user.controller.js
+++ entitys/user/controller/user.controller.js
@@ -1,4 +1,4 @@
-import User from '../user.Schema.js';
+import User from "../user.Schema.js";
 
 const userController = {};
 
@@ -9,11 +9,23 @@ userController.emailRegister = async (req, res, next) => {
 
     // 비밀번호 유효성 검사, 이름 유효성 검사, 이메일 유효성 검사 추가 예정
 
-    if (password !== passwordCheck) throw new Error('비밀번호 확인이 일치하지 않습니다.');
+    if (password !== passwordCheck) throw new Error("비밀번호 확인이 일치하지 않습니다.");
   } catch (e) {
     req.statusCode = 400;
     req.error = e.message;
   }
   next();
 };
+
+// 닉네임 검증
+userController.nicknameValid = async (req, res, next) => {
+  try {
+    const { nickname } = req.body;
+  } catch (e) {
+    req.statusCode = 400;
+    req.error = e.message;
+  }
+  next();
+};
+
 export default userController;
